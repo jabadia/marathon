e27dd15e916bd08afcd25971909bfcdd5da6c75a
Javier Abadia <javi.abadia@gmail.com>
2013-07-15 00:27:24 +0200

    comprobar que los ids almacenados en las cookies siguen existiendo realmente

--- 1client/js/app.js
+++ 1client/js/app.js
@@ -1,9 +1,23 @@
 "use strict";
 
-var app = angular.module('training',['trainingFilters','userService','competitionService', 'ngCookies']);
+var app = angular.module('training',['trainingFilters','userService','competitionService', 'planService','ngCookies']);
 
-app.run(function($rootScope, $cookies)
-{
-	$rootScope.selectedUserId = $cookies.selectedUserId || {};
-	$rootScope.selectedCompetitionId = $cookies.selectedCompetitionId || {};
+app.run(function($rootScope, $cookies, User, Competition, Plan)
+{	
+	// check that the stored ids still exist
+	User.get({ uid:  $cookies.selectedUserId || ""}, function(user)
+	{
+		$rootScope.selectedUserId = user._id;
+	});
+
+	Competition.get({ cid: $cookies.selectedCompetitionId || ""}, function(competition)
+	{
+		$rootScope.selectedCompetitionId = competition._id;
+	});
+
+	Plan.get({ pid: $cookies.selectedPlanId || ""}, function(plan)
+	{
+		$rootScope.selectedPlanId = plan._id;
+	});
+
 });
